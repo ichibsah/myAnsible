c7c6bcdbc3c11269c78c4a53689334190cdda5f9
ibrahim <ibrahim@virthost01.skyline.lan>
2023-08-22 12:34:45 +0200

    Tue Aug 22 12:34:45 CEST 2023

---
+++ git.sh
@@ -0,0 +1,36 @@
+
+#git remote add master git@github.com:ichibsah/myAnsible.git
+
+#git push --force --set-upstream master master
+
+# https://github.com/ichibsah/myAnsible
+# git@github.com:ichibsah/myAnsible.git
+#git push <name>
+
+#…or push an existing repository from the command line
+#git remote add origin git@github.com:ichibsah/myAnsible.git
+#git branch -M main
+#git branch -M master
+#git push -u origin main
+#git push -u origin master
+
+
+git add .
+git commit -am "$(date)"
+git push
+#git push master master -f
+
+
+# echo "# myAnsible" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin git@github.com:ichibsah/myAnsible.git
+# git push -u origin main
+
+
+# OR push an existing repository from the command line
+# git remote add origin git@github.com:ichibsah/myAnsible.git
+# git branch -M main
+# git push -u origin main
