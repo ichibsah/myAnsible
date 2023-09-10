5481d60704c2000f473cf08869cf06cd9b2a6b36
ibrahim <ibrahim@de-1005-gw>
2023-09-10 07:37:38 +0200

    Sun Sep 10 07:37:38 CEST 2023

--- test.sh
+++ test.sh
@@ -9,7 +9,8 @@ ansible-inventory -y --list
 #ansible-playbook -v run-dockers.yml # works
 ###ansible-playbook -v --tags test run-main.yml # works
 #ansible-playbook -v --tags test -c local run-main.yml # works
-ansible-playbook -v -i test-inv.yml run-main.yml # works
+#ansible-playbook -v -i test-inv.yml run-main.yml # works
+ansible-playbook -v --tags test -i test-inv.yml run-main.yml # works
 #ansible-playbook -v --tags test run-provisions.yml # works
 #ansible-playbook -v run-anydesk.yml # works
 #ansible-playbook -v run-apt.yml # works
