75d0a770532b5ec8cf44177a02014b9042df20f3
ibrahim <ibrahim@de-1005-gw.skyline.lan>
2024-01-21 17:03:41 +0100

    Sun Jan 21 17:03:41 CET 2024

---
+++ win_update.sh
@@ -0,0 +1,12 @@
+clear
+#
+ansible-inventory -y --list
+#
+ansible-playbook -v run-windows.yml # works
+#
+#
+# things that have to be setup on windows pcs for this to work
+
+# https://winscp.net/eng/docs/guide_windows_openssh_server
+
+# https://adamtheautomator.com/openssh-windows/
