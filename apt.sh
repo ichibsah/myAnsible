0fd480924dfd59fa6a98cf6aa1ba814e82df9fde
ibrahim <ibrahim@de-1005-gw.skyline.lan>
2024-01-23 11:00:57 +0100

    Tue Jan 23 11:00:57 CET 2024

---
+++ apt.sh
@@ -0,0 +1,9 @@
+clear
+# docker build -t docker-agent-ansible:latest . 
+# docker image tag docker-agent-ansible:latest docker.io/michibsah/docker-agent-ansible:latest
+# docker login -u michibsah
+# docker push docker.io/michibsah/docker-agent-ansible:latest
+#
+ansible-inventory -y --list
+#
+ansible-playbook -v run-apt.yml # works
