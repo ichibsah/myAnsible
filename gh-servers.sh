0f5d7f3c911058f35cee2d169f9a645a5540f0e0
ibrahim <ibrahim@de-1005-gw.skyline.lan>
2024-07-10 08:27:09 +0200

    Wed Jul 10 08:27:09 CEST 2024

--- gh-servers.sh
+++ gh-servers.sh
@@ -1,10 +1,8 @@
 clear
-# docker build -t docker-agent-ansible:latest . 
-# docker image tag docker-agent-ansible:latest docker.io/michibsah/docker-agent-ansible:latest
-# docker login -u michibsah
-# docker push docker.io/michibsah/docker-agent-ansible:latest
 #
 ansible-inventory -y --list
 #
 #ansible-playbook -v run-main.yml # works
-ansible-playbook gh-servers run-main.yml # works
+#ansible-playbook --limit "gh-servers" run-main.yml # works
+ansible-playbook -v --limit gh-servers run-main.yml --list-host # works
+#ansible-playbook -v --limit gh-servers run-main.yml # works
