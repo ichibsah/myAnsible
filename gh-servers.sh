136503c2d1d5b1ee320a26bb3f108312f338a37e
ibrahim <ibrahim@de-1005-gw.skyline.lan>
2024-07-10 08:32:23 +0200

    Wed Jul 10 08:32:23 CEST 2024

--- gh-servers.sh
+++ gh-servers.sh
@@ -4,5 +4,5 @@ ansible-inventory -y --list
 #
 #ansible-playbook -v run-main.yml # works
 #ansible-playbook --limit "gh-servers" run-main.yml # works
-ansible-playbook -v --limit gh-servers run-main.yml --list-host # works
-#ansible-playbook -v --limit gh-servers run-main.yml # works
+#ansible-playbook -v --limit gh-servers run-main.yml --list-host # works
+ansible-playbook -v --limit gh-servers run-main.yml # works
