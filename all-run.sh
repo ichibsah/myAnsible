d7285b20c2e1d89281b460081ce739ef05c8924b
ibrahim <ibrahim@de-1005-gw.skyline.lan>
2024-07-12 09:56:11 +0200

    Fri Jul 12 09:56:11 CEST 2024

--- all-run.sh
+++ all-run.sh
@@ -6,5 +6,5 @@ clear
 #
 ansible-inventory -y --list
 #
-ansible-playbook -v --limit !gh-servers run-main.yml --list-host # works
-#ansible-playbook -v --limit !gh-servers run-main.yml # works
+#ansible-playbook -v --limit !gh-servers run-main.yml --list-host # works
+ansible-playbook -v --limit !gh-servers run-main.yml # works
