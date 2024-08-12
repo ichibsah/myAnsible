aeb9d7b93727a010e3dd80a2c48abc4bcfc0fd57
ibrahim <ibrahim@gh-1017-gw.skyline.lan>
2024-08-12 11:54:38 +0200

    Mon Aug 12 11:54:38 CEST 2024

---
+++ ionos.sh
@@ -0,0 +1,10 @@
+clear
+# docker build -t docker-agent-ansible:latest . 
+# docker image tag docker-agent-ansible:latest docker.io/michibsah/docker-agent-ansible:latest
+# docker login -u michibsah
+# docker push docker.io/michibsah/docker-agent-ansible:latest
+#
+ansible-inventory -y --list
+#
+#ansible-playbook -v --limit !gh-servers run-main.yml --list-host # works
+ansible-playbook -v --limit ionos run-main.yml # works
