e646b704504cecf11c583c314161a5e22ce3b2b7
ibrahim <ibrahim@gh-1017-gw.skyline.lan>
2025-07-29 08:59:03 +0200

    Tue Jul 29 08:59:03 CEST 2025

--- gh-servers.sh
+++ gh-servers.sh
@@ -1,12 +1,16 @@
 clear
 #
-ansible-inventory -y --list
+#ansible-inventory -y --list
+#
+mkdir -p /home/ibrahim/log/
+touch /home/ibrahim/log/ansible_log.yml
+chmod 666 /home/ibrahim/log/ansible_log.yml
 #
 #ansible-playbook -v run-main.yml # works
 #ansible-playbook --limit "gh-servers" run-main.yml # works
 #ansible-playbook -v --limit gh-servers run-main.yml --list-host # works
-ansible-playbook -v --limit gh-servers run-main.yml # works
+ansible-playbook --limit gh-servers run-main.yml # works
 #ansible-playbook -v gh-servers run-main.yml # works
 #
-ansible-galaxy collection install -r requirements.yml
-ansible-galaxy collection install community.healthchecksio --upgrade
+#ansible-galaxy collection install -r requirements.yml
+#ansible-galaxy collection install community.healthchecksio --upgrade
