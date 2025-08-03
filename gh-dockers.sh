f37fcebb9ed27ea8fb81552f4d73578212dfc897
ibrahim <ibrahim@de-1005-gw.skyline.lan>
2025-08-03 17:40:59 +0200

    Sun Aug  3 17:40:59 CEST 2025

---
+++ gh-dockers.sh
@@ -0,0 +1,10 @@
+clear
+# docker build -t docker-agent-ansible:latest . 
+# docker image tag docker-agent-ansible:latest docker.io/michibsah/docker-agent-ansible:latest
+# docker login -u michibsah
+# docker push docker.io/michibsah/docker-agent-ansible:latest
+#
+ansible-inventory -y --list
+#
+ansible-playbook -v --limit gh-servers run-dockers.yml # works
+#ansible-playbook -v run-dockers.yml # works
