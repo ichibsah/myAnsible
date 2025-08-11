fb20663d63166434543d957500eadcc8b1e127d3
ibrahim <ibrahim@de-1005-gw.skyline.lan>
2025-08-11 16:03:57 +0200

    Mon Aug 11 16:03:57 CEST 2025

--- configs/files/iptables/gw-gh.sh
+++ configs/files/iptables/gw-gh.sh
@@ -1,5 +1,8 @@
 #ip route delete default
+echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+#
 sudo route add default gw 192.168.1.1
 ping -w 3 8.8.8.8 -I eno1   # LAN
 ping -w 3 8.8.8.8 -I enp2s0 # WLAN
 ping -w 3 8.8.8.8 -I enp5s0 # WAN
+#
