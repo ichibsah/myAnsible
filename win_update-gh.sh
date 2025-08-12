c7117a6ea3700060ebfe977b2dc0cd181bc7fe33
ibrahim <ibrahim@de-1005-gw.skyline.lan>
2025-08-12 19:10:29 +0200

    Tue Aug 12 19:10:29 CEST 2025

---
+++ win_update-gh.sh
@@ -0,0 +1,12 @@
+clear
+#
+ansible-inventory -y --list
+#
+ansible-playbook -v --limit gh-windows_users run-windows.yml # works
+#
+#
+# things that have to be setup on windows pcs for this to work
+
+# https://winscp.net/eng/docs/guide_windows_openssh_server
+
+# https://adamtheautomator.com/openssh-windows/
